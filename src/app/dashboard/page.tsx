0ed0e5c158ea5da820de4afbc1bebe76a35c7216
yamil-tasshamjit <yamil.m@tasshamjit.com>
2024-06-25 13:34:55 +0530

    data mapping  with database

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -78,13 +78,56 @@ import {
 import { TooltipProvider } from "@radix-ui/react-tooltip"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import prisma from "@/lib/db";
 
+async function getData({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+}
 export async function Dashboard() {
   const { getUser } =getKindeServerSession();
   const user = await getUser();
     if(!user){
       return redirect('/')
     } 
+    await getData({
+      email: user.email as string,
+      firstName: user.given_name as string,
+      id: user.id as string,
+      lastName: user.family_name as string,
+      profileImage: user.picture,
+    });
+
   return (
     
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
