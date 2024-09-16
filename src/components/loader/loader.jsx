1c93104b0c8e54aa239f33f2bc6079da15fe0fc9
nandu <nandu.g@tasshamjit.com>
2024-09-16 11:02:18 +0530

    chatbot page implemented

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,13 @@
+import { CirclesWithBar } from "react-loader-spinner";
+
+import React from 'react'
+
+function Loader() {
+  return (
+    <div>
+      <CirclesWithBar color="white" size="10"/>
+    </div>
+  )
+}
+
+export default Loader
